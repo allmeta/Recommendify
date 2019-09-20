1f0734cc2e6e3dd7fd4404ca78b98b34d6236bae
thomas <meta@DESKTOP-G8MOC2D.localdomain>
2019-09-20 13:39:43 +0200

    store token

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
+import moment from "moment";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Artist from "./components/artist";
@@ -7,11 +8,20 @@ import Search from "./components/search";
 import Authorize from "./components/auth";
 
 const App = props => {
-  const [token, setToken] = useState(undefined);
+  const [token, setToken] = useState(localStorage.getItem("access_token"));
+  const tokenExpired = () => {
+    let exp = localStorage.getItem("exp");
+    return !exp || moment(exp).diff(moment()) < 0;
+  };
+  useEffect(() => {
+    if (!token || tokenExpired()) {
+      //token not found or expired
+      props.history.push("/auth");
+    }
+  }, []);
   return (
-    <article>
+    <React.Fragment>
       <Header />
-      <div>{token}</div>
       <main>
         <Switch>
           <Route exact path="/" component={() => <Search token={token} />} />
@@ -26,7 +36,7 @@ const App = props => {
         </Switch>
       </main>
       <Footer />
-    </article>
+    </React.Fragment>
   );
 };
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
 
 const Search = ({ token }) => {
-  return token ? (
-    <section>main page nice you can search here ehehe</section>
-  ) : (
-    <Redirect to="/auth" />
-  );
+  return <section>{token}</section>;
 };
 
 export default Search;
